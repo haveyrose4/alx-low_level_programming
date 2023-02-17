fb4ea05a35ea4ac7c86fcd4ac2d6756e3d1765b0
Haveyrose4 <chiamakaclara691999@gmail.com>
2023-02-16 16:08:10 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char Mystring[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystring);
+	return (0);
+}	
